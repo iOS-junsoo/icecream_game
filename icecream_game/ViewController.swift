ba9d0a388ad8a24f87bbe93c4c0316ea433bccb6
kimjunsoo <rlawnstn01023@naver.com>
2021-10-03 18:25:29 +0900

    Initial Commit

---
+++ icecream_game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  icecream_game
+//
+//  Created by 준수김 on 2021/10/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
